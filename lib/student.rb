3c5bfda399b74a9e3580bd37c3e0efe277b6052b
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-26 21:01:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,10 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+attr_accessor :name , :grade
+
+def initialize(name, grade, id = NIL)
+  @name = name
+  @grade = grade 
+  
   
 end
