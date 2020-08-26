04a3f88d5c8203a84d1cbbc04b6a79059e16b4c8
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-26 21:05:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,11 +22,7 @@ def self.create_table
   
 def self.drop_table 
    sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        album TEXT
-        )
+      
         SQL
     DB[:conn].execute(sql) 
   end
