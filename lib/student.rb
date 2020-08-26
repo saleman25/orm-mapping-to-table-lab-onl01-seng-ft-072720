a039b49db46ab1d588001d8751bd563d43c48220
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-26 21:02:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,15 @@
 class Student
 
 attr_accessor :name , :grade
+attr_reader :id
 
 def initialize(name, grade, id = NIL)
   @name = name
   @grade = grade 
   @id = id
+end 
+
+def self.create_table
+  
   
 end
